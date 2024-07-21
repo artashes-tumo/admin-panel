a5656a9dfa96768bd1eea9ea61e245d4ecad56d0
Zareh <zareh.melkonyan@tumo.org>
2024-07-21 13:10:16 +0400

    Lesson 6 Roues

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,10 +13,15 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-
     <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.dataTables.css" />
+    <script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @yield('head')
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
@@ -23,7 +29,9 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -50,14 +58,15 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                    <a class="dropdown-item" href="{{ route('member_index') }}">Members</a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                             document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
@@ -77,4 +86,5 @@
         </main>
     </div>
 </body>
+
 </html>
--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -1,33 +1,57 @@
 @extends('layouts.app')
 
+@section('head')
+<style>
+    .delete-button {
+        margin-left: 10px;
+    }
+</style>
+@endsection
+
 @section('content')
 <div class="container">
-    <table>
-        <tr>
-            <th>Id</th>
-            <th>Email</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Gender</th>
-            <th>Age</th>
-            <th>Job Titile</th>
-            <th>Salary</th>
-            <th>Joined at</th>
-        </tr>
-        @foreach ($members as $member)
-        <tr>
-            <td>{{$member->id}}</td>
-            <td>{{$member->email}}</td>
-            <td>{{$member->first_name}}</td>
-            <td>{{$member->last_name}}</td>
-            <td>{{$member->gender}}</td>
-            <td>{{$member->age}}</td>
-            <td>{{$member->job_title}}</td>
-            <td>{{$member->salary}}</td>
-            <td>{{$member->joined_at}}</td>
-        </tr>
-
-        @endforeach
+    <div class="button-content">
+        <a href="{{route('member_create')}}" class="btn btn-sm btn-primary">Add Member</a>
+    </div>
+    <table id="members_list_table" class="display">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Email</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Gender</th>
+                <th>Age</th>
+                <th>Job Title</th>
+                <th>Salary</th>
+                <th>Joined At</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($members as $member)
+                <tr>
+                    <td>{{ $member->id }}</td>
+                    <td>{{ $member->email }}</td>
+                    <td>{{ $member->first_name }}</td>
+                    <td>{{ $member->last_name }}</td>
+                    <td>{{ $member->gender }}</td>
+                    <td>{{ $member->age }}</td>
+                    <td>{{ $member->position }}</td>
+                    <td>{{ $member->sallary }}</td>
+                    <td>{{ Carbon\Carbon::parse($member->joined_at)->format('d-m-Y') }}</td>
+                    <td>
+                        <a class="btn btn-sm btn-primary" href="{{route('member_edit', ['id'=>$member->id])}}">Edit</a>
+                        <a class="btn btn-sm btn-danger delete-button" href="">Delete</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
     </table>
 </div>
+<script>
+    $(document).ready(() => {
+        $('#members_list_table').DataTable({});
+    })
+</script>
 @endsection
